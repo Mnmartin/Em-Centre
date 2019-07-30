92c0834475d6047777284d14f22b975a4674d146
Martin Muhia <martin.muhia10@gmail.com>
2019-07-30 11:21:46 +0300

    ads

--- Scripts/New Patient Registration/Verify General Information/Script1564391124709.groovy
+++ Scripts/New Patient Registration/Verify General Information/Script1564391124709.groovy
@@ -13,3 +13,65 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser(GlobalVariable.url)
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('General information/new_patient_regi btn'))
+
+WebUI.sendKeys(findTestObject('General information/input_fname'), fname)
+
+WebUI.sendKeys(findTestObject('General information/input_mname'), mname)
+
+WebUI.sendKeys(findTestObject('General information/input_lname'), lname)
+
+WebUI.click(findTestObject('General information/select_gender'))
+
+WebUI.click(findTestObject('General information/d.o.b'))
+
+WebUI.click(findTestObject('General information/year of birth'))
+
+WebUI.click(findTestObject('General information/month of birth'))
+
+WebUI.click(findTestObject('General information/date of birth'))
+
+WebUI.sendKeys(findTestObject('General information/input_ssn'), ssn)
+
+WebUI.click(findTestObject('General information/input_marital status'))
+
+WebUI.sendKeys(findTestObject('General information/input_address'), address1)
+
+WebUI.sendKeys(findTestObject('General information/input_address2'), address2)
+
+WebUI.sendKeys(findTestObject('General information/input_city'), city)
+
+WebUI.click(findTestObject('General information/input_state'))
+
+WebUI.sendKeys(findTestObject('General information/input_zip'), zip)
+
+WebUI.sendKeys(findTestObject('General information/input_email'), email)
+
+WebUI.sendKeys(findTestObject('General information/input_main_mobile'), mobile)
+
+WebUI.sendKeys(findTestObject('General information/input_mobile'), mobile)
+
+WebUI.sendKeys(findTestObject('General information/input_work mobile'), mobile)
+
+WebUI.sendKeys(findTestObject('General information/input_pharmacy name'), pharmacy_name)
+
+WebUI.sendKeys(findTestObject('General information/input_pharmacy No'), mobile)
+
+WebUI.sendKeys(findTestObject('General information/input_pharmacy address'), pharmacy_address)
+
+WebUI.sendKeys(findTestObject('General information/input_contact name'), emergency_contact)
+
+WebUI.sendKeys(findTestObject('General information/input_relationship'), emergency_relationship)
+
+WebUI.sendKeys(findTestObject('General information/input_emergency no'), emergency_no)
+
+WebUI.click(findTestObject('General information/input_ethnic'))
+
+WebUI.click(findTestObject('General information/input_language'))
+
+WebUI.click(findTestObject('General information/general_next btn'))
+
